d22162034c170853f593c47e5f726091ce615f98
Jingchuan Lin <lin@Jingchuans-MacBook-Pro.local>
2018-09-04 11:23:38 +0800

    Use numpy instead

--- assignment1/q2/a1q2_solution.py
+++ assignment1/q2/a1q2_solution.py
@@ -1,132 +1,11 @@
 import csv
-import math
-from random import seed
-from random import random
+import numpy as np
 
-seed(1)
-
-# help function to read inputs in csv files
-def read_csv(filename):
-    res = []
-    with open(filename, newline='') as csvfile:
-        file = csv.reader(csvfile)
-        for row in file:
-            res.append(row)
-    return res
-
-# help function for lists multiplication
-def mul_list(list1, list2):
-    if (len(list1) != len(list2)):
-        print("Input lists have different lengths")
-        return None
-    res = list()
-    for i in range(0, len(list1)):
-        res.append(list1[i]*list2[i])
-    return res
-#print(mul_list([1,2,3], [1,0,1]))
-#-> [1, 0, 3]
-
-# help function for matrix transpose
-def transpose_mat(mat):
-    res = list()
-    row, col = len(mat), len(mat[0])
-    for n in range(0, col):
-        temp = list()
-        for m in range(0, row):
-            temp.append(mat[m][n])
-        res.append(temp)
-    return res
-
-# help function for matrix scalar multiplication
-def sca_mat(mat, scalar):
-    res = list()
-    row, col = len(mat), len(mat[0])
-    for m in range(0, row):
-        temp = list()
-        for n in range(0, col):
-            temp.append(mat[m][n]*scalar)
-        res.append(temp)
-    return res
-#print(sca_mat([[1,2,3], [1,1,4]],2))
-#-> [[2, 4, 6], [2, 2, 8]]
-
-# help function for matrices addition
-def add_mat(mat1, mat2):
-    mat1_row, mat1_col = len(mat1), len(mat1[0])
-    mat2_row, mat2_col = len(mat2), len(mat2[0])
-    # check the dimensions of input matrices
-    if (mat1_row != mat2_row) or (mat1_col != mat2_col):
-        print("Incorrect dimentions of input matrices")
-        return None
-    # calculate the add
-    res = [[0 for col in range(0, mat1_col)] for row in range(0, mat1_row)]
-    for i in range(0, mat1_row):
-        for j in range(0, mat1_col):
-            res[i][j] += mat1[i][j] + mat2[i][j]
-    return res
-#print(add_mat([[1,2,3]], [[0,1,0]]))
-#-> [[1, 3, 3]]
-#print(add_mat([1,3,4],[0,2,1]))
-#-> error
-
-# help function for matrices substraction
-def sub_mat(mat1, mat2):
-    mat1_row, mat1_col = len(mat1), len(mat1[0])
-    mat2_row, mat2_col = len(mat2), len(mat2[0])
-    # check the dimensions of input matrices
-    if (mat1_row != mat2_row) or (mat1_col != mat2_col):
-        print("Incorrect dimentions of input matrices")
-        return None
-    # calculate the add
-    res = [[0 for col in range(0, mat1_col)] for row in range(0, mat1_row)]
-    for i in range(0, mat1_row):
-        for j in range(0, mat1_col):
-            res[i][j] += mat1[i][j] - mat2[i][j]
-    return res
-#print(sub_mat([[1,2,3]], [[0,1,0]]))
-#-> [[1, 1, 3]]
-
-# help function for matrices multiplication
-def mul_mat(mat1, mat2):
-    mat1_row, mat1_col = len(mat1), len(mat1[0])
-    mat2_row, mat2_col = len(mat2), len(mat2[0])
-    # check the dimensions of input matrices
-    if mat1_col != mat2_row:
-        print("Incorrect dimentions of input matrices")
-        return None
-    # calculate the multiplication 
-    res = [[0 for row in range(0, mat2_col)] for col in range(0, mat1_row)]
-    for i in range(0, mat1_row):
-        for j in range(0, mat2_col):
-            for k in range(0, mat1_col):
-                res[i][j] += mat1[i][k] * mat2[k][j]
-    return res
-#t_inputs = [[1,2]]
-#t_weights = [[0.5,1,2], [1,0.5,0.5]]
-#print(mul_mat(t_inputs, t_weights))
-#-> [[2.5, 2.0, 3.0]]
-    
-
-# help function to create the identity matrix
-def identity_mat(n):
-    res = list()
-    for i in range(n):
-        temp = []
-        for j in range(n):
-            if i==j:
-                temp.append(1)
-            else:
-                temp.append(0)
-        res.append(temp)
-    return res
-#print(identity_mat(2))
-#-> [[1, 0], [0, 1]]
-
-# initialize a neural network layer
 def init_layer(n_inputs, n_outputs):
-    w = [[random() for i in range(n_outputs)] for i in range(n_inputs)]
-    b = [[random() for i in range(n_outputs)]]
+    w = np.random.randn(n_inputs, n_outputs)/np.sqrt(n_outputs)
+    b = np.random.randn(n_outputs)
     return {'weights': w, 'bias': b}
+#print(init_layer(2,3))
 
 # initialize a neural network
 def init_nn(n_inputs, n_outputs, *n_hidden):
@@ -142,10 +21,11 @@ def init_nn(n_inputs, n_outputs, *n_hidden):
             network.append(init_layer(n_hidden[n], n_hidden[n+1]))
         network.append(init_layer(n_hidden[-1], n_outputs))
     return network
+nn = init_nn(2,3,4,3)
 
 # define the linear activation function
 def linear_activate(inputs, weights, bias):
-    res = add_mat(mul_mat(inputs, weights), bias)
+    res = np.add(np.dot(inputs, weights), bias)
     return res
 #t_inputs = [[1,2]]
 #t_weights = [[0.5,1,2], [1,0.5,0.5]]
@@ -155,40 +35,35 @@ def linear_activate(inputs, weights, bias):
 
 # define the ReLu transfer function
 def relu_transfer(x):
-    res = [max(0,i) for i in x]
-    return res
+    return np.maximum(x, 0)
 
-# calculate the derivative of Relu
+## calculate the derivative of Relu
 def relu_transfer_derivative(x):
-    res = list()
-    for i in x:
-        if i>0:
-            res.append(1)
-        else:
-            res.append(0)
-    return res
+    x[x<=0] = 0
+    x[x>0] = 1
+    return x
 #print(relu_transfer_derivative([-1,2,3,-8]))
 #-> [0, 1, 1, 0]
 
 # define the softmax transfer function
 def softmax_transfer(x):
-    x_exp = [math.exp(i) for i in x]
-    res = [i/sum(x_exp) for i in x_exp]
-    return res
-#print(softmax_transfer([-1,1,2,3]))
+    e_x = np.exp(x - np.max(x))
+    e_x_sum = e_x.sum(axis=0)
+    return e_x / e_x_sum
+#print(softmax_transfer(np.array([-1,1,2,3])))
 #-> [0.012037642711939451, 0.08894681729740428, 0.24178251715880078, 0.6572330228318555]
 
 # calculate the derivative of softmax
 def softmax_transfer_derivative(x):
-    x_exp = [math.exp(i) for i in x]
-    res = [i*(sum(x_exp)-i)/(sum(x_exp)**2) for i in x_exp]
-    return res
+    e_x = np.exp(x - np.max(x))
+    e_x_sum = e_x.sum(axis=0)
+    return e_x*(e_x_sum-e_x)/(e_x_sum**2)
 #print(softmax_transfer_derivative([1,2,3,4]))
 #-> [0.03103084923581511, 0.0795501864530196, 0.18076934858369267, 0.22928868580089717]
 
 # forward feeding function 
 def forward_feeding(inputs, network):
-    res = inputs.copy()
+    res = inputs
     for n in range(len(network)):
         layer = network[n]
         res = linear_activate(res, layer['weights'], layer['bias'])
@@ -199,95 +74,93 @@ def forward_feeding(inputs, network):
         layer['output'] = res
     return res
 
-### testting
-def transfer_derivative(output):
-	return output * (1.0 - output)
-
-# Backpropagate error and store in neurons
-def backward_propagate_error(network, expected):
-	for i in reversed(range(len(network))):
-		layer = network[i]
-		errors = list()
-		if i != len(network)-1:
-			for j in range(len(layer)):
-				error = 0.0
-				for neuron in network[i + 1]:
-					error += (neuron['weights'][j] * neuron['delta'])
-				errors.append(error)
-		else:
-			for j in range(len(layer)):
-				neuron = layer[j]
-				errors.append(expected[j] - neuron['output'])
-		for j in range(len(layer)):
-			neuron = layer[j]
-			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
- 
-# test backpropagation of error
-network = [[{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
-		[{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]}, {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
-expected = [0, 1]
-backward_propagate_error(network, expected)
-#for layer in network:
-#	print(layer)
-#-> [{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614], 'delta': -0.0005348048046610517}]
-#-> [{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095], 'delta': -0.14619064683582808}, {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763], 'delta': 0.0771723774346327}]
-
-def sigmoid_transfer_derivative(x):
-    res = [i * (1.0 - i) for i in x]
-    return res
-#print(sigmoid_transfer_derivative([0.1,0.2,0.3]))
-#-> [0.09000000000000001, 0.16000000000000003, 0.21]
-
 # backward propagate function
 def backward_propagate(network, expected):
     for n in reversed(range(len(network))):
         layer = network[n]
-        errors = list()
+        #errors = list()
         if n==(len(network)-1):
-            errors = sub_mat(expected, layer['output'])
+            errors = np.subtract(expected, layer['output'])
             derivative = softmax_transfer_derivative(layer['output'][0])
         else:
             layer_plus = network[n+1]
-            errors = mul_mat(layer_plus['delta'],
-                             transpose_mat(layer_plus['weights']))
+            errors = np.dot(layer_plus['delta'], layer_plus['weights'].T)
             derivative = relu_transfer_derivative(layer['output'][0])
-        delta = mul_list(errors[0], derivative)
+        delta = np.multiply(errors[0], derivative)
         layer['delta'] = [delta]
     pass
 
-network = [{'weights':[[0.13436424411240122], [0.8474337369372327]], 'bias':[[0.763774618976614]], 'output':[[0.7105668883115941]]},
-            {'weights':[[0.2550690257394217, 0.4494910647887381]], 'bias':[[0.49543508709194095, 0.651592972722763]], 'output':[[0.6213859615555266, 0.6573693455986976]]}]
-expected = [[0, 1]]
-backward_propagate(network, expected)
-#for layer in network:
-#    print(layer)
-#-> {'weights': [[0.13436424411240122], [0.8474337369372327]], 'bias': [[0.763774618976614]], 'output': [[0.7105668883115941]], 'delta': [[-0.0005348048046610517]]}
-#-> {'weights': [[0.2550690257394217, 0.4494910647887381]], 'bias': [[0.49543508709194095, 0.651592972722763]], 'output': [[0.6213859615555266, 0.6573693455986976]], 'delta': [[-0.14619064683582808, 0.0771723774346327]]}
-
 # update the neural network weights
 def update_weights(network, inputs, l_rate):
-#    print("update_weights")
-#    print(network)
     for n in range(len(network)):
         if n!=0:
             inputs = network[n-1]['output']
-#        print(inputs)
-#        print(network[n]['weights'])
-#        print("DELTA")
-#        print(network[n]['delta'])
-        temp = sca_mat(mul_mat(transpose_mat(inputs),
-                               network[n]['delta']), l_rate)
-#        print(temp)
-        new_weights = add_mat(temp,network[n]['weights'])
-        temp = sca_mat(network[n]['delta'], l_rate)
-        new_bias = add_mat(temp,network[n]['bias'])
+        temp = np.multiply(np.dot(inputs.T, network[n]['delta']),l_rate)
+        new_weights = np.add(temp, network[n]['weights'])
+        temp = np.multiply(network[n]['delta'],l_rate)
+        new_bias = np.add(temp, network[n]['bias'])
         network[n]['weights'] = new_weights
-#        print("new weights")
-#        print(new_weights)
-#        print(network[n]['weights'])
         network[n]['bias'] = new_bias
-#    print(network)
     return None
+
+def cross_entropy_cost(pred, actual):
+    return -(np.sum(np.multiply(actual, np.log10(pred))) +
+             np.sum(np.multiply((1-actual), np.log10(1-pred))))
+#p = np.array([0.98, 0.01, 0.01])
+#a = np.array([1.0, 0.0, 0.0])
+#print(np.multiply(a, np.log10(p)))
+#print(np.multiply((1-a), np.log10(1-p)))
+#print(cross_entropy_cost(p,a))
+#-> 0.6632621566107613
+
+# train the neural network
+def train_nn(network, x_train, y_train, x_test, y_test, l_rate, n_epoch):
+    res = {'stat':[], 'trained_nn': None}
+    for epoch in range(n_epoch):
+        train_n_inputs = len(x_train)
+        train_error_sum = 0
+        train_correct = 0
+        test_n_inputs = len(x_test)
+        test_error_sum = 0
+        test_correct = 0
+        for m in range(train_n_inputs):
+            current_input = x_train[m]
+            pred_output = forward_feeding(current_input, network)
+            actual_output = y_train[m]
+            train_error_sum += cross_entropy_cost(pred_output[0],
+                                                  actual_output[0])
+            if np.argmax(pred_output[0]) == np.argmax(actual_output[0]):
+                train_correct += 1
+            backward_propagate(network, actual_output)
+            update_weights(network, current_input, l_rate)
+        for n in range(test_n_inputs):
+            current_input = x_test[n]
+            pred_output = forward_feeding(current_input, network)
+            actual_output = y_test[n]
+            test_error_sum += cross_entropy_cost(pred_output[0],
+                                                 actual_output[0])
+            if np.argmax(pred_output[0]) == np.argmax(actual_output[0]):
+                test_correct += 1
+        rec = ('>epoch=%d, lrate=%.3f, train_error=%.3f, train_acc=%.3f, ' +
+               'test_error=%.3f, test_acc=%.3f') % (epoch+1, l_rate,
+                train_error_sum/train_n_inputs, train_correct/train_n_inputs,
+                test_error_sum/test_n_inputs, test_correct/test_n_inputs)
+        print(rec)
+        res['stat'].append([rec])
+    res['trained_nn'] = network
+    return res
+
+# help function to read inputs in csv files
+def read_x_csv(filename):
+    res = []
+    with open(filename, newline='') as csvfile:
+        file = csv.reader(csvfile)
+        for row in file:
+            rec = list()
+            for e in row:
+                rec.append(int(e))
+            res.append([rec])
+    return res
 
 # one hot encoding
 def one_hot_encoding(inputs, n_class):
@@ -299,99 +172,77 @@ def one_hot_encoding(inputs, n_class):
                 temp.append(1)
             else:
                 temp.append(0)
-        res.append(temp)
+        res.append([temp])
     return res
-#one_hot_encoding([1,2,3,0,1],4)
-#-> [[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [1, 0, 0, 0], [0, 1, 0, 0]]
+#print(one_hot_encoding([1,2,3,0,1],4))
+#-> [[[0, 1, 0, 0]], [[0, 0, 1, 0]], [[0, 0, 0, 1]], [[1, 0, 0, 0]], [[0, 1, 0, 0]]]
+    
+def read_y_csv(filename):
+    res = list()
+    with open(filename, newline='') as csvfile:
+        file = csv.reader(csvfile)
+        for row in file:
+            for e in row:
+                res.append(int(e))
+    return res
 
-# calculate cross entropy cost
-def cross_entropy_cost(pred_res, actual_res):
-    cost = 0
-    n = len(pred_res)
-    for i in range(n):
-        cost += (actual_res[i]*math.log(pred_res[i])
-                 + (1-actual_res[i])*math.log(1-pred_res[i]))
-    cost = (-1/n)*cost
-    return cost
-#print(cross_entropy_cost([0.3541008934987469, 0.1548374930445365, 0.18574050171086504, 0.30532111174585147],
-#                         [0,0,1,0]))
-#-> 0.6632621566107613
+def write_csv(filename, res):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for w in res:
+            writer.writerow(w)
+    
+# Part 1
+np.random.seed(11)
+x_train = np.array(read_x_csv('x_train.csv'))
+y_train = np.array(one_hot_encoding(read_y_csv('y_train.csv'), 4))
+x_test = np.array(read_x_csv('x_test.csv'))
+y_test = np.array(one_hot_encoding(read_y_csv('y_test.csv'), 4))
+network_a = init_nn(14, 4, 100, 40)
+#train_nn(network_a, x_train[0:10], y_train[0:10], 0.001, 10)
+#res_a = train_nn(network_a, x_train, y_train, x_test, y_test, 0.001,600)
+#write_csv('network_a_output_stat.csv', res_a['stat'])
 
-def square_error(pred_res, actual_res):
-    cost = 0
-    n = len(pred_res)
-    for i in range(n):
-        cost += (actual_res[i]-pred_res[i])**2
-    return cost
-#print(square_error([1,2,3],[1,1,2]))
-#-> 2
+network_b = init_nn(14, 4, 28, 28, 28, 28, 28, 28, 40)
+#train_nn(network_b, x_train[0:10], y_train[0:10], 0.01, 10)
+res_b = train_nn(network_b, x_train, y_train, x_test, y_test, 0.001, 200)
+write_csv('network_b_output_stat_600.csv', res_b['stat'])
 
-## train the neural network
-#def train_nn_backup(network, train_inputs, n_outputs, l_rate, n_epoch):
-#    for epoch in range(n_epoch):
-#        sum_error = 0
-#        for ipt in train_inputs:
-#            outputs = forward_feeding(ipt, network)
-#            expected = [[0 for i in range(n_outputs)]]
-#            #error = square_error(outputs[0], expected[0])
-#            error = cross_entropy_cost(outputs[0], expected[0])
-#            sum_error += error
-#            backward_propagate(network, expected)
-#            update_weights(network, ipt, l_rate)
-#        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+#network_c = init_nn(14, 4, 14, 14, 14, 14, 14, 14, 14,
+#                    14, 14, 14, 14, 14, 14, 14,
+#                    14, 14, 14, 14, 14, 14, 14,
+#                    14, 14, 14, 14, 14, 14, 14)
+##train_nn(network_c, x_train[0:10], y_train[0:10], 0.01, 10)
+#train_nn(network_c, x_train, y_train, 0.001, 10)
 
-# train the neural network
-def train_nn(network, train_inputs, expected_outputs, l_rate, n_epoch):
-    for epoch in range(n_epoch):
-        sum_error = 0
-        for n in range(len(train_inputs)):
-            outputs = forward_feeding(train_inputs[n], network)
-            real = expected_outputs[n]
-            error = cross_entropy_cost(outputs[0], real[0])
-            sum_error += error
-            backward_propagate(network, real)
-            update_weights(network, train_inputs[n], l_rate)
-        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+# Part 2
+def read_wb_csv(filename, ):
+    res = {}
+    with open(filename, newline='') as csvfile:
+        file = csv.reader(csvfile)
+        for row in file:
+            key = row[0]
+            if key not in res.keys():
+                res[key] = []
+            res[key].append([float(e) for e in row[1:]])
+    return res
 
-#ntk = init_nn(2, 1, 2)
-#ti = [[[1,2]],[[1,1]],[[3,0]]]
-#n_outputs = 1
-#l_rate = 0.5
-#n_epoch = 1
-#train_nn(ntk, ti, n_outputs, l_rate, n_epoch)
+#X=[-1, 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1]
+#label = [3]
+#weights = read_wb_csv('./b/w-100-40-4.csv')
+#bias = read_wb_csv('./b/b-100-40-4.csv')
+#nn = list()
+#for i in range(len(weights.keys())):
+#    layer = {}
+#    layer['weights'] = weights[list(weights.keys())[i]]
+#    layer['bias'] = bias[list(bias.keys())[i]]
+#    nn.append(layer)
+#ipt = [X]
+#opt = forward_feeding(ipt, nn)
+#actual = one_hot_encoding(label, 4)[0]
+#error = cross_entropy_cost(opt[0], actual[0])
 
-dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
-n_inputs = len(dataset[0]) - 1
-n_outputs = len(set([row[-1] for row in dataset]))
-network = init_nn(n_inputs, 2, n_outputs)
-train_inputs = list()
-for rec in dataset:
-    train_inputs.append([rec[0:2]])
-expected_opts = list()
-for rec in dataset:
-    expected_opts.append([[0,1]])
-train_nn(network, train_inputs, expected_opts, 0.001, 50)
-#train_nn_backup(network, train_inputs, n_outputs, 0.001, 50)
-#for layer in network:
-#	print(layer)
+#train_nn(nn, [X], one_hot_encoding(label, 4), 0.001, 10)
 
-
-nn1 = init_nn(3, 4, 5)
-nn1o = forward_feeding([[1,1,1]], nn1)
-#nn1o2 = forward_feeding([[0,1,0]], nn1, 'softmax')
-#print(nn1o)
-#print(nn1o2)
-
-#t1 = init_nn(2,2)
-#tr = forward_feeding([[1,2]], t1, 'softmax')
 
     
